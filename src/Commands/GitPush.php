5529a10cb2138f9d15b4c18a43c4e2fa62fd8966
Raúl Mauricio Uñate Castro <91748598+rmunate@users.noreply.github.com>
2022-07-06 11:22:22 -0500

    V1.1

--- src/Commands/GitPush.php
+++ src/Commands/GitPush.php
@@ -29,7 +29,7 @@ class GitPush extends Command{
     |
     */
     protected $signature = 'GitPush {rama} {--m=}';
-    protected $description = 'Ejecutar Git Push al repositorio con la opción de ejecutar pull de una Rama especifica creada en el repositorio.';
+    protected $description = 'Ejecutar Git Push al repositorio desde una rama puesta como parámetro, con la opción de ejecutar Pull de una Rama especifica creada en el repositorio.';
 
     /*
     |--------------------------------------------------------------------------
@@ -41,20 +41,35 @@ class GitPush extends Command{
     */
     public function handle(){
 
-        $this->line('Start GitPush Artisan Utilites');
+        $this->line('Start GitPush Artisan Utilities');
+
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
+        #                       PARTE 1 - GIT STATUS                        #
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
 
         // Estatus del proyecto
         $this->info('Estado Actualización Proyecto...');
-        $this->line(ArtisanCommandClass::GitStatus());
+        $gitStatus = ArtisanCommandClass::GitStatus();
+        $this->line($gitStatus);
 
         /* LLamado Comando Personalizado */
-        $this->info('Haremos una limpieza de su proyecto antes de cargarlo en GIT para garantizar que no se cargue información que puede generar conflicto con otra rama.');
+        $this->info('Haremos una limpieza del proyecto antes de cargarlo en GIT para garantizar que no se cargue información que puede generar conflicto con otra rama.');
         ArtisanCommandClass::CallArtisan('FlushCache');
         $this->info('Limpieza Completa y Exitosa!');
 
         /* Argumentos Funcion */
         $rama = $this->argument('rama');
+
+        /* Si la rama enviada como argumento no existe, se detendra el proceso */
+        if(!ArtisanCommandClass::branchValidate($rama)){
+            return $this->error('La rama ingresada como parámetro no es la misma sobre la cual se está trabajando actualmente, por favor revise y vuelva a ejecutar el comando.');
+        };
+
         $hora = date("Y-m-d H:i:s");
+
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
+        #                       PARTE 2 - GIT ADD                           #
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
 
         /* GitAdd. */
         $this->info('Ejecutando...');
@@ -62,46 +77,9 @@ class GitPush extends Command{
         ArtisanCommandClass::GitAdd();
         ArtisanCommandClass::ProcessingTime(3);
 
-        // Definir si existe comentarios para el Commit
-        if (is_string($this->option()["m"])) {
-
-            /* Se Valida la longitud de la Observacion. */
-            if (strlen($this->option()["m"]) > 2) {
-
-                ArtisanCommandClass::CallArtisan('FlushCache');
-
-                /* De tener mas de dos Caracteres, se usará el texto como comentario del commmit */
-                $comentarioCommit = $this->option()["m"];
-                $this->info('Ejecutando...');
-                $this->line('git commit -m "' . $comentarioCommit . '"');
-                ArtisanCommandClass::GitCommit($comentarioCommit);
-                ArtisanCommandClass::ProcessingTime(3);
-
-            } else {
-
-                ArtisanCommandClass::CallArtisan('FlushCache');
-
-                /* De tener menos de 2 caracteres, se usará el datatime como comentario del commmit */
-                $this->info('El comentario asociado tiene menos de 2 caracteres, por lo cual no se usará en el Cambio a cargar, usaremos la fecha y hora actual.');
-                $this->info('Ejecutando...');
-                $this->line('git commit -m "Update ' . $hora . '"');
-                ArtisanCommandClass::GitCommit($hora);
-                ArtisanCommandClass::ProcessingTime(3);
-
-            }
-
-        } else {
-
-            ArtisanCommandClass::CallArtisan('FlushCache');
-
-            /* De no haberse ingresado un comentario en el comando, se usará el datetime como comentario del commmit */
-            $this->info('Se usará la hora y fecha como comentario del Cambio, en la ausencia de un comentario personalizado');
-            $this->info('Ejecutando...');
-            $this->line('git commit -m "Update ' . $hora . '"');
-            ArtisanCommandClass::GitCommit($hora);
-            ArtisanCommandClass::ProcessingTime(3);
-
-        }
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
+        #                       PARTE 3 - GIT PULL                          #
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
 
         /* Ramas Proyecto | Conocer las Ramas Asociadas al Proyecto. */
         $ramas = ArtisanCommandClass::GitBranch();
@@ -118,15 +96,14 @@ class GitPush extends Command{
             /* Cantidad de Ramas */
             $cantidadRamas = count($ramasFinal);
 
-            $preguntaPull = 'undefined';
-            while ($preguntaPull <> 'No') {
+            $preguntaPull = null;
+            while ($preguntaPull == null) {
 
                 $this->info('Este Proyecto Tiene Un Total De ' . $cantidadRamas . ' Rama(s) En GIT');
 
-                $preguntaPull = $this->choice(
-                    'Deseas Hacer Pull De Una Rama',
-                    ['No', 'Si']
-                );
+                $preguntaPull = $this->choice('Deseas Hacer Pull De Una Rama',[
+                    'No', 'Si'
+                ]);
 
                 if ($preguntaPull == 'Si') {
 
@@ -148,6 +125,63 @@ class GitPush extends Command{
 
         ArtisanCommandClass::CallArtisan('FlushCache');
 
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
+        #                       PARTE 3 - GIT COMMIT                        #
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
+
+        // Definir si existe comentarios para el Commit
+        if (is_string($this->option()["m"])) {
+
+            /* Se Valida la longitud de la Observacion. */
+            if (strlen($this->option()["m"]) > 2) {
+
+                ArtisanCommandClass::CallArtisan('FlushCache');
+
+                /* De tener mas de dos Caracteres, se usará el texto como comentario del commmit */
+                $comentarioCommit = $this->option()["m"];
+                $this->info('Ejecutando...');
+                $this->line('git commit -m "' . $comentarioCommit . '"');
+                ArtisanCommandClass::GitCommit($comentarioCommit);
+                ArtisanCommandClass::ProcessingTime(3);
+
+            } else {
+
+                ArtisanCommandClass::CallArtisan('FlushCache');
+                /* De tener menos de 2 caracteres, se usará el datatime como comentario del commmit */
+                $this->info('El comentario asociado tiene menos de 2 caracteres, por lo cual no se usará en el Cambio a cargar, Registraremos el nombre del(los) archivo(s) modificado(s) de existir, de lo contrario la hora y fecha.');
+                $this->info('Ejecutando...');
+
+                /* Comentario */
+                $comment = ArtisanCommandClass::comment($gitStatus);
+
+                /* Git Commit */
+                $this->line('git commit -m "' . $comment . '"');
+                ArtisanCommandClass::GitCommit($comment);
+                ArtisanCommandClass::ProcessingTime(3);
+            }
+
+        } else {
+
+            ArtisanCommandClass::CallArtisan('FlushCache');
+
+            /* De no haberse ingresado un comentario en el comando, se usará el datetime como comentario del commmit */
+            $this->info('Registraremos el nombre del(los) archivo(s) modificado(s) de existir, de lo contrario la hora y fecha, en la ausencia de un comentario personalizado');
+            $this->info('Ejecutando...');
+
+            /* Comentario */
+            $comment = ArtisanCommandClass::comment($gitStatus);
+
+            /* Git Commit */
+            $this->line('git commit -m "' . $comment . '"');
+            ArtisanCommandClass::GitCommit($comment);
+            ArtisanCommandClass::ProcessingTime(3);
+
+        }
+
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
+        #                       PARTE 3 - GIT PUSH                          #
+        #▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬#
+
         $this->info('Ejecutando...');
         $this->line('git push origin ' . $this->argument('rama'));
         ArtisanCommandClass::GitPush($this->argument('rama'));
@@ -157,7 +191,7 @@ class GitPush extends Command{
         $this->info('Proceso ejecutado con Exito!');
 
         /* Mensaje de Cierre */
-        $this->line('End GitPush Artisan Utilites');
+        $this->line('End GitPush Artisan Utilities');
 
     }
 }
