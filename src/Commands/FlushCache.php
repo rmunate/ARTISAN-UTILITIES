757a0e8d928c716183e73307767576d14a111bc4
Raúl Mauricio Uñate Castro <91748598+rmunate@users.noreply.github.com>
2022-07-21 08:56:44 -0500

    V 2.0.0

--- src/Commands/FlushCache.php
+++ src/Commands/FlushCache.php
@@ -7,95 +7,78 @@
 |
 | Optimización de la Eliminacion de todos los caches de Laravel 8^.
 | Autor: Raul Mauricio Uñate Castro
-| Fecha: 20-12-2021
+| V 1.0: 20-12-2021
+| V 1.2: 01-05-2022
+| V 2.0: 19-07-2022 (Reescrito)
+|--------------------------------------------------------------------------
 |
 */
 
 namespace App\Console\Commands;
 
-use Artisan;
 use Illuminate\Console\Command;
-use App\Console\ArtisanCommandClass;
+use App\Console\ArtisanUtilities;
 
-class FlushCache extends Command{
+class FlushCache extends Command {
 
-    /*
-    |--------------------------------------------------------------------------
-    | Identificación Comando.
-    |--------------------------------------------------------------------------
-    |
-    | $signature = Nombre para Consola.
-    | description = Descripción.
-    |
-    */
-
+    /**
+     * Nombre del Comando
+     * @var string
+     */
     protected $signature = 'FlushCache';
-    protected $description = 'Ejecuta la limpieza de los Caches de [Laravel, Configuración, Vistas, Rutas, Toquen Caducados] y eliminación del LOG de errores del proyecto.';
 
-    /*
-    |--------------------------------------------------------------------------
-    | Incio Ejecución de Comando.
-    |--------------------------------------------------------------------------
-    |
-    | Logaritmo Comando.
-    |
-    */
+    /**
+     * Descripción del Comando
+     * @var string
+     */
+    protected $description = 'Ejecuta la limpieza de los Caches de Laravel[Configuración, Vistas, Rutas, Toquen Caducados entre otros] adicional elimina el LOG de errores del proyecto.';
 
+    /**
+     * Codigo del Comando
+     * @return Void
+     */
     public function handle(){
 
-        $this->line('Start FlushCache Artisan Utilities');
+        /* Omitir Errores */
+        ArtisanUtilities::errorHidden();
+
+        $this->line(ArtisanUtilities::$start);
         $this->info('Inicio limpieza del Proyecto');
 
-        /* Ajuste Storage | Logs */
+        /* Ajuste Storage & Logs */
         $this->info("Inicio Revisión Estructura Storage.");
-        ArtisanCommandClass::DeleteLogs();
-        $this->info("Revisión Estructura Storage Completa y corregida.");
-        $this->info("Log del Proyecto Vaciado Correctamente.");
+        ArtisanUtilities::DefaultStorage();
+        $this->info("Revisión Estructura Storage Completa y Corregida.");
+        $this->info("Log de Errores del Proyecto Vaciado Correctamente.");
 
         /* Eliminacion Cache del proyecto */
-        @ArtisanCommandClass::CallArtisan('cache');
-        $this->info("Cache Eliminado del Proyecto Correctamente");
-
-        /* Eliminacion Cache de Configuracion */
-        @ArtisanCommandClass::CallArtisan('config');
-        $this->info("Cache de Configuración Eliminado del Proyecto Correctamente");
-
-        /* Eliminacion Cache de Vistas */
-        @ArtisanCommandClass::CallArtisan('view');
-        $this->info("Cache de Vistas Eliminado del Proyecto Correctamente");
-
-        /* Eliminacion Cache de Rutas */
-        @ArtisanCommandClass::CallArtisan('route');
-        $this->info("Cache de Rutas Eliminado del Proyecto Correctamente");
-
-        /* Eliminacion Cache de Tokkens */
-        @ArtisanCommandClass::CallArtisan('auth');
-        $this->info("Cache de Tokens Caducados Eliminado del Proyecto Correctamente en base de datos");
-
-        /* Eliminacion Cache de Eventos */
-        @ArtisanCommandClass::CallArtisan('event');
-        $this->info("Cache de Eventos Eliminado del Proyecto Correctamente");
-
-        /* Eliminacion Cache de Eventos */
-        @ArtisanCommandClass::CallArtisan('permission');
-        $this->info("Cache de Permisos Eliminado del Proyecto Correctamente");
-
-        /* Eliminacion Cache de Queue */
-        @ArtisanCommandClass::CallArtisan('queue');
-        $this->info("Cache de Queue Eliminado del Proyecto Correctamente");
-
-        /* Eliminacion Cache de Schedule */
-        @ArtisanCommandClass::CallArtisan('schedule');
-        $this->info("Cache de Schedule Eliminado del Proyecto Correctamente");
-
-        /* Optimizar */
-        @ArtisanCommandClass::CallArtisan('optimize');
-        $this->info("Proyecto Optimizado al 100%");
-
-        /* Mensaje Final */
-        $this->info('Proceso ejecutado con Exito!');
+        /**
+         * Comandos Artisan a Ejecutar
+         * Mismo Orden de Ejecucion
+         * @var array
+         */
+        $commands = [
+            'cache' => 'Cache Eliminado del Proyecto Correctamente',
+            'config' => 'Cache de Configuración Eliminado del Proyecto Correctamente',
+            'view' => 'Cache de Vistas Eliminado del Proyecto Correctamente',
+            'route' => 'Cache de Rutas Eliminado del Proyecto Correctamente',
+            'auth' => 'Cache de Tokens Caducados Eliminado del Proyecto Correctamente en base de datos',
+            'event' => 'Cache de Eventos Eliminado del Proyecto Correctamente',
+            'permission' => 'Cache de Permisos Eliminado del Proyecto Correctamente',
+            'queue' => 'Cache de Cola Eliminado del Proyecto Correctamente',
+            'schedule' => 'Cache de Calendario Eliminado del Proyecto Correctamente',
+            'optimize' => 'Proyecto Optimizado'
+        ];
+        foreach ($commands as $command => $comment) {
+            @ArtisanUtilities::Call($command);
+            $this->info($comment);
+        }
 
         /* Cierre */
-        $this->line('End FlushCache Artisan Utilities');
+        $this->info(ArtisanUtilities::$last);
+        $this->line(ArtisanUtilities::$end);
+
+        /* Activacion Errores */
+        ArtisanUtilities::errorShow();
     }
 }
